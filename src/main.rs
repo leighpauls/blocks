ea05764cd7fe9d5ee68a435afefa79a74d41be6c
Leigh Pauls <leighpauls@Leighs-MacBook-Pro.local>
2019-06-24 00:01:03 -0700

    replace tutorial stuff with a basic field

--- src/main.rs
+++ src/main.rs
@@ -9,55 +9,74 @@ use quicksilver::{
     Result,
 };
 
+const FIELD_WIDTH: usize = 10;
+const FIELD_HEIGHT: usize = 22;
+
+const BLOCK_SIZE: f32 = 20.0;
+
+#[derive(Copy, Clone)]
+enum FieldBlock {
+    Empty,
+    Occupied,
+}
+
+type Field = [[FieldBlock; FIELD_HEIGHT]; FIELD_WIDTH];
+
 struct Screen {
-    position: Vector,
-    image: Asset<Image>,
     is_first_loop: bool,
+    field: Field,
 }
 
 impl State for Screen {
     fn new() -> Result<Screen> {
         Ok(Screen {
-            position: Vector::new(50, 50),
-            image: Asset::new(Image::load("image.png")),
             is_first_loop: true,
+            field: [[FieldBlock::Empty; FIELD_HEIGHT]; FIELD_WIDTH],
         })
     }
 
     fn draw(&mut self, window: &mut Window) -> Result<()> {
         window.clear(Color::WHITE)?;
-        let pos = self.position;
-        self.image.execute(|loaded_image| {
-            window.draw_ex(
-                &Rectangle::new(pos, (100, 200)),
-                Img(loaded_image),
-                Transform::translate((0, 200)) * Transform::rotate(45),
-                0,
-            );
-            Ok(())
-        })
-    }
-
-    fn update(&mut self, window: &mut Window) -> Result<()> {
-        if self.is_first_loop {
-            println!(
-                "Focused! update rate: {} draw rate: {} max updates: {}",
-                window.update_rate(),
-                window.draw_rate(),
-                window.max_updates()
-            );
-            self.is_first_loop = false;
+        for y in 0..FIELD_HEIGHT {
+            for x in 0..FIELD_WIDTH {
+                window.draw(
+                    &Rectangle::new(
+                        (BLOCK_SIZE * x as f32, BLOCK_SIZE * y as f32),
+                        (BLOCK_SIZE, BLOCK_SIZE),
+                    ),
+                    match self.field[x][y] {
+                        FieldBlock::Empty => Color::BLUE,
+                        FieldBlock::Occupied => Color::RED,
+                    },
+                );
+            }
         }
         Ok(())
     }
 
-    fn event(&mut self, event: &Event, window: &mut Window) -> Result<()> {
+    fn update(&mut self, window: &mut Window) -> Result<()> {
+        if self.is_first_loop {
+            self.is_first_loop = false;
+            println!(
+                "Focused! update rate: {} draw rate: {} max updates: {}",
+                window.update_rate(),
+                window.draw_rate(),
+                window.max_updates(),
+            );
+
+            self.field[3][4] = FieldBlock::Occupied;
+        }
+        Ok(())
+    }
+
+    fn event(&mut self, event: &Event, _window: &mut Window) -> Result<()> {
         if let Event::Key(key, ButtonState::Pressed) = event {
-            self.position.x += match key {
-                Key::Left => -50.0,
-                Key::Right => 50.0,
-                _ => 0.0,
-            }
+            let target = &mut self.field[5][6];
+            *target = match key {
+                Key::Left => FieldBlock::Occupied,
+                Key::Right => FieldBlock::Empty,
+                _ => *target,
+            };
         }
         Ok(())
     }
