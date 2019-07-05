72069f22575ceddcf124d9a8fd34731fa4404264
Leigh Pauls <leighpauls@Leighs-MacBook-Pro.local>
2019-07-04 23:54:09 -0700

    flesh out rotations of I

--- src/shapes.rs
+++ src/shapes.rs
@@ -18,12 +18,17 @@ pub enum Rotations {
 }
 
 pub fn shape_positions(shape: Shape, rotations: Rotations) -> RelativePoses {
+    use Rotations::*;
+    use Shape::*;
     match (shape, rotations) {
-        (Shape::O, _) => [p(1, 1), p(2, 1), p(1, 2), p(2, 2)],
-        (Shape::I, Rotations::Zero) => [p(0, 2), p(1, 2), p(2, 2), p(3, 2)],
-        (Shape::I, _) => [p(0, 2), p(1, 2), p(2, 2), p(3, 2)],
+        (O, _) => [p(1, 1), p(2, 1), p(1, 2), p(2, 2)],
+
+        (I, Zero) => [p(0, 2), p(1, 2), p(2, 2), p(3, 2)],
+        (I, One) => [p(2, 0), p(2, 1), p(2, 2), p(2, 3)],
+        (I, Two) => [p(0, 1), p(1, 1), p(2, 1), p(3, 1)],
+        (I, Three) => [p(1, 0), p(1, 1), p(1, 2), p(1, 3)],
 
         #[cfg(test)]
-        (Shape::TestShape, _) => [p(0, 0); 4],
+        (TestShape, _) => [p(0, 0); 4],
     }
 }
