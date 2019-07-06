72f78c068478fbd20b75be21881fb76f7ac4f960
Leigh Pauls <leighpauls@Leighs-MacBook-Pro.local>
2019-07-05 19:51:03 -0700

    flesh out all the block types

--- src/shapes.rs
+++ src/shapes.rs
@@ -2,30 +2,66 @@ use crate::position::{p, Pos, RelativePoses, RotateDir, Rotations};
 use num_traits::FromPrimitive;
 use rand::random;
 
-const NUM_SHAPES: usize = 2;
-
 pub trait ShapeDef {
     fn positions(&self, rotation: Rotations) -> RelativePoses;
     fn wall_kick_offsets(&self, initial_rot: Rotations, rot_dir: RotateDir) -> Vec<Pos>;
 }
 
+const NUM_SHAPES: usize = 7;
+
 #[derive(Copy, Clone, FromPrimitive)]
 pub enum Shape {
     I,
     O,
+    J,
+    L,
+    S,
+    Z,
+    T,
 }
 
 impl ShapeDef for Shape {
     fn positions(&self, rotation: Rotations) -> RelativePoses {
         use Rotations::*;
         use Shape::*;
-        match (self, rotation) {
-            (O, _) => [p(1, 2), p(2, 2), p(1, 3), p(2, 3)],
-
-            (I, Zero) => [p(0, 2), p(1, 2), p(2, 2), p(3, 2)],
-            (I, One) => [p(2, 0), p(2, 1), p(2, 2), p(2, 3)],
-            (I, Two) => [p(0, 1), p(1, 1), p(2, 1), p(3, 1)],
-            (I, Three) => [p(1, 0), p(1, 1), p(1, 2), p(1, 3)],
+        match self {
+            O => [p(1, 2), p(2, 2), p(1, 3), p(2, 3)],
+            I => match rotation {
+                Zero => [p(0, 2), p(1, 2), p(2, 2), p(3, 2)],
+                One => [p(2, 0), p(2, 1), p(2, 2), p(2, 3)],
+                Two => [p(0, 1), p(1, 1), p(2, 1), p(3, 1)],
+                Three => [p(1, 0), p(1, 1), p(1, 2), p(1, 3)],
+            },
+            J => match rotation {
+                Zero => [p(0, 3), p(0, 2), p(1, 2), p(2, 2)],
+                One => [p(1, 1), p(1, 2), p(1, 3), p(2, 3)],
+                Two => [p(0, 2), p(1, 2), p(2, 2), p(2, 1)],
+                Three => [p(0, 1), p(1, 1), p(1, 2), p(1, 3)],
+            },
+            L => match rotation {
+                Zero => [p(2, 3), p(0, 2), p(1, 2), p(2, 2)],
+                One => [p(1, 1), p(1, 2), p(1, 3), p(2, 1)],
+                Two => [p(0, 2), p(1, 2), p(2, 2), p(0, 1)],
+                Three => [p(0, 3), p(1, 1), p(1, 2), p(1, 3)],
+            },
+            S => match rotation {
+                Zero => [p(0, 2), p(1, 2), p(1, 3), p(2, 3)],
+                One => [p(1, 3), p(1, 2), p(2, 2), p(2, 1)],
+                Two => [p(0, 1), p(1, 1), p(1, 2), p(2, 2)],
+                Three => [p(0, 3), p(0, 2), p(1, 2), p(1, 1)],
+            },
+            Z => match rotation {
+                Zero => [p(0, 3), p(1, 3), p(1, 2), p(2, 2)],
+                One => [p(1, 1), p(1, 2), p(2, 2), p(2, 3)],
+                Two => [p(0, 2), p(1, 2), p(1, 1), p(2, 1)],
+                Three => [p(0, 1), p(0, 2), p(1, 2), p(1, 3)],
+            },
+            T => match rotation {
+                Zero => [p(0, 2), p(1, 2), p(1, 3), p(2, 2)],
+                One => [p(1, 3), p(1, 2), p(1, 1), p(2, 2)],
+                Two => [p(0, 2), p(1, 2), p(2, 2), p(1, 1)],
+                Three => [p(0, 2), p(1, 1), p(1, 2), p(1, 3)],
+            },
         }
     }
 
@@ -45,6 +81,16 @@ impl ShapeDef for Shape {
             (I, One, CCW) => vec![p(0, 0), p(2, 0), p(-1, 0), p(2, 1), p(-1, -2)],
             (I, Two, CCW) => vec![p(0, 0), p(1, 0), p(-2, 0), p(1, -2), p(-2, 1)],
             (I, Three, CCW) => vec![p(0, 0), p(-2, 0), p(1, 0), p(-2, -1), p(1, 2)],
+
+            (_, Zero, CW) => vec![p(0, 0), p(-1, 0), p(-1, 1), p(0, -2), p(-1, -2)],
+            (_, One, CW) => vec![p(0, 0), p(1, 0), p(1, -1), p(0, 2), p(1, 2)],
+            (_, Two, CW) => vec![p(0, 0), p(1, 0), p(1, 1), p(0, -2), p(1, -2)],
+            (_, Three, CW) => vec![p(0, 0), p(-1, 0), p(-1, -1), p(0, 2), p(-1, 2)],
+
+            (_, Zero, CCW) => vec![p(0, 0), p(1, 0), p(1, 1), p(0, -2), p(1, -2)],
+            (_, One, CCW) => vec![p(0, 0), p(1, 0), p(1, -1), p(0, 2), p(1, 2)],
+            (_, Two, CCW) => vec![p(0, 0), p(-1, 0), p(-1, 1), p(0, -2), p(-1, -2)],
+            (_, Three, CCW) => vec![p(0, 0), p(-1, 0), p(-1, -1), p(0, 2), p(-1, 2)],
         }
     }
 }
