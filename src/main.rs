95fb91417ed8429ce25a48899745aeb78f733625
Leigh Pauls <leighpauls@Leighs-MacBook-Pro.local>
2019-07-20 21:46:19 -0700

    Move drawing code to render.rs

--- src/main.rs
+++ src/main.rs
@@ -29,20 +29,18 @@ mod time;
 use futures::Async;
 use gamestate::GameState;
 use quicksilver::{
-    geom::{Shape, Transform, Vector},
-    graphics::{Background::Img, Color, Font, FontStyle},
+    geom::Vector,
+    graphics::{Font},
     input::{ButtonState, Key},
     lifecycle::{run, Event, Settings, State, Window},
     Error, Future, Result,
 };
-use render::{render_blocks, BlockRenderInstructions};
+use render::draw_field;
 
-const BLOCK_SIZE_RATIO: f32 = 0.04;
-
-struct Game {
-    state: GameState,
-    screen_size: Vector,
-    score_font: Font,
+pub struct Game {
+    pub state: GameState,
+    pub screen_size: Vector,
+    pub score_font: Font,
 }
 
 type FontFuture = Box<Future<Item = Font, Error = Error>>;
@@ -93,71 +91,7 @@ impl State for GameWrapper {
             }
         };
 
-        window.clear(Color::WHITE)?;
-
-        let screen_size = game.screen_size;
-        let full_height = screen_size.y;
-        let block_size = BLOCK_SIZE_RATIO * full_height;
-
-        let render_info = game.state.render_info();
-
-        let scale_transform = Transform::scale((block_size, block_size));
-        let position_transform = Transform::translate((
-            screen_size.x * 0.5
-                - (0.5 * block_size * render_info.playing_field.width_blocks() as f32),
-            screen_size.y * 0.5
-                - (0.5 * block_size * render_info.playing_field.height_blocks() as f32),
-        )) * scale_transform;
-
-        render_blocks(
-            &render_info.playing_field,
-            scale_transform,
-            position_transform,
-            window,
-        );
-
-        let preview_block_size = 0.03 * full_height;
-        let preview_scale_transform = Transform::scale((preview_block_size, preview_block_size));
-        let preview_root_position =
-            Transform::translate((screen_size.x * 0.7, screen_size.y * 0.2))
-                * preview_scale_transform;
-        for (i, shape) in render_info.previews.iter().enumerate() {
-            render_blocks(
-                &*shape,
-                preview_scale_transform,
-                preview_root_position * Transform::translate((0, 3 * i as i32)),
-                window,
-            );
-        }
-
-        if let Some(hold_piece) = render_info.hold_piece {
-            let hold_piece_position =
-                Transform::translate((screen_size.x * 0.22, screen_size.y * 0.25))
-                    * preview_scale_transform;
-            render_blocks(
-                &hold_piece,
-                preview_scale_transform,
-                hold_piece_position,
-                window,
-            );
-        }
-
-        let style = FontStyle::new(24.0, Color::BLACK);
-        let score_image = game.score_font.render(
-            &format!(
-                "Lines: {}\nLevel: {}",
-                render_info.cleared_lines, render_info.level
-            ),
-            &style,
-        )?;
-        window.draw(
-            &score_image
-                .area()
-                .translate((screen_size.x * 0.7, screen_size.y * 0.7)),
-            Img(&score_image),
-        );
-
-        Ok(())
+        draw_field(window, &game)
     }
 
     fn update(&mut self, window: &mut Window) -> Result<()> {
--- src/render.rs
+++ src/render.rs
@@ -1,11 +1,14 @@
 use crate::position::Coord;
 use crate::position::Pos;
 use crate::shapes::Shape;
+use crate::Game;
 
 use quicksilver::{
     geom::{Rectangle, Transform, Vector},
-    graphics::Color,
+    graphics::{Color, FontStyle},
     lifecycle::Window,
+    prelude::Background::Img,
+    Result,
 };
 
 pub enum DrawBlockType {
@@ -53,7 +56,7 @@ where
     fn width_blocks(&self) -> Coord;
 }
 
-pub fn render_blocks<T, I>(
+fn render_blocks<T, I>(
     instructions: &T,
     scale_transform: Transform,
     position_transform: Transform,
@@ -77,4 +80,72 @@ pub fn render_blocks<T, I>(
             0,
         );
     }
+}
+
+const BLOCK_SIZE_RATIO: f32 = 0.04;
+
+pub fn draw_field(window: &mut Window, game: &Game) -> Result<()> {
+    window.clear(Color::WHITE)?;
+
+    let screen_size = &game.screen_size;
+    let full_height = screen_size.y;
+    let block_size = BLOCK_SIZE_RATIO * full_height;
+
+    let render_info = game.state.render_info();
+
+    let scale_transform = Transform::scale((block_size, block_size));
+    let position_transform = Transform::translate((
+        screen_size.x * 0.5 - (0.5 * block_size * render_info.playing_field.width_blocks() as f32),
+        screen_size.y * 0.5 - (0.5 * block_size * render_info.playing_field.height_blocks() as f32),
+    )) * scale_transform;
+
+    render_blocks(
+        &render_info.playing_field,
+        scale_transform,
+        position_transform,
+        window,
+    );
+
+    let preview_block_size = 0.03 * full_height;
+    let preview_scale_transform = Transform::scale((preview_block_size, preview_block_size));
+    let preview_root_position =
+        Transform::translate((screen_size.x * 0.7, screen_size.y * 0.2)) * preview_scale_transform;
+    for (i, shape) in render_info.previews.iter().enumerate() {
+        render_blocks(
+            &*shape,
+            preview_scale_transform,
+            preview_root_position * Transform::translate((0, 3 * i as i32)),
+            window,
+        );
+    }
+
+    if let Some(hold_piece) = render_info.hold_piece {
+        let hold_piece_position =
+            Transform::translate((screen_size.x * 0.22, screen_size.y * 0.25))
+                * preview_scale_transform;
+        render_blocks(
+            &hold_piece,
+            preview_scale_transform,
+            hold_piece_position,
+            window,
+        );
+    }
+
+    let style = FontStyle::new(24.0, Color::BLACK);
+    let score_image = game.score_font.render(
+        &format!(
+            "Lines: {}\nLevel: {}",
+            render_info.cleared_lines, render_info.level
+        ),
+        &style,
+    )?;
+    use quicksilver::geom::Shape;
+    window.draw(
+        &score_image
+            .area()
+            .translate((screen_size.x * 0.7, screen_size.y * 0.7)),
+        Img(&score_image),
+    );
+
+    Ok(())
 }
